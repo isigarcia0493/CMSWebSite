2752143b4dc312ff076219eadab080516932866b
Isigarcia0493 <ig705295@southeast.edu>
2023-03-04 07:31:42 -0600

    Edit roles working properly

--- Controllers/AdministratorController.cs
+++ Controllers/AdministratorController.cs
@@ -1,6 +1,9 @@
-﻿using CMSWebsite.ViewModels;
+﻿using CMSWebsite.Models;
+using CMSWebsite.ViewModels;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace CMSWebsite.Controllers
@@ -8,10 +11,12 @@ namespace CMSWebsite.Controllers
     public class AdministratorController : Controller
     {
         private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly UserManager<ApplicationUser> _userManager;
 
-        public AdministratorController(RoleManager<IdentityRole> roleManager)
+        public AdministratorController(RoleManager<IdentityRole> roleManager, UserManager<ApplicationUser> userManager)
         {
             _roleManager = roleManager;
+            _userManager = userManager;
         }
 
         [HttpGet]
@@ -34,7 +39,7 @@ namespace CMSWebsite.Controllers
 
                 if(result.Succeeded)
                 {
-                    return RedirectToAction("Index", "Home");
+                    return RedirectToAction("GetRoles");
                 }
 
                 foreach(IdentityError error in result.Errors)
@@ -44,6 +49,73 @@ namespace CMSWebsite.Controllers
             }
 
             return View(model);
+        }
+
+        public IActionResult GetRoles()
+        {
+            var roles = _roleManager.Roles.ToList();
+
+            return View(roles);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> EditRole(string id)
+        {
+            var role = await _roleManager.FindByIdAsync(id);
+
+            if(role == null)
+            {
+                ViewBag.ErrorMessage = $"Role has not been Found";
+
+                return View("Error");
+            }
+            
+            var model = new EditRoleViewModel
+            {
+                RoleId = id,
+                RoleName = role.Name
+            };
+
+            foreach(var user in _userManager.Users)
+            {
+                if(await _userManager.IsInRoleAsync(user, role.Name))
+                {
+                    model.Users.Add(user.UserName);
+                }
+            }
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> EditRole(EditRoleViewModel model)
+        {
+            var role = await _roleManager.FindByIdAsync(model.RoleId);
+
+            if (role == null)
+            {
+                ViewBag.ErrorMessage = $"Role has not been Found";
+
+                return View("Error");
+            }
+            else
+            {
+                role.Name = model.RoleName;
+                var result = await _roleManager.UpdateAsync(role);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("GetRoles");
+                }
+                else
+                {
+                    foreach(var error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error.Description);
+                    }
+                    return View(model);
+                }
+            }
         }
     }
 }
---
+++ ViewModels/EditRoleViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace CMSWebsite.ViewModels
+{
+    public class EditRoleViewModel
+    {
+        public EditRoleViewModel() 
+        {
+            Users = new List<string>();
+        }
+
+        public string RoleId { get; set; }
+
+        [Required(ErrorMessage = "Please entr a role Name")]
+        [DisplayName("Role Name")]
+        public string RoleName { get; set; }
+
+        public List<string> Users { get; set; }
+    }
+}
