bdeca8212b883c75cfb61c1b834bfc954a0c0e1b
Isigarcia0493 <ig705295@southeast.edu>
2023-03-28 20:04:49 -0500

    Admins site updated

--- Areas/Admin/Controllers/DashboardController.cs
+++ Areas/Admin/Controllers/DashboardController.cs
@@ -1,7 +1,11 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using CMSWebsite.Areas.Admin.ViewModels;
+using CMSWebsite.ServiceInterfaces;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System.Data;
+using System.Linq;
+using System.Threading;
 
 namespace CMSWebsite.Areas.Admin.Controllers
 {
@@ -10,10 +14,35 @@ namespace CMSWebsite.Areas.Admin.Controllers
     [Authorize(Roles = "Admin")]
     public class DashboardController : Controller
     {
+        private readonly IAlbumService _albumService;
+        private readonly ICategoryService _categoryService;
+        private readonly IEventService _eventService;
+        private readonly IImageService _imageService;
+        private readonly IFormMessageService _formMessageService;
+
+        public DashboardController(IAlbumService albumService, ICategoryService categoryService, 
+                                   IEventService eventService, IImageService imageService, IFormMessageService formMessageService)
+        {
+            _albumService = albumService;
+            _categoryService = categoryService;
+            _eventService = eventService;
+            _imageService = imageService;
+            _formMessageService = formMessageService;
+        }
+
         [HttpGet]
         public ActionResult Index()
         {
-            return View();
+            DashboardViewModel dashboardVM = new DashboardViewModel()
+            {
+                Album = _albumService.GetAllAlbums().ToList(),
+                Category = _categoryService.GetAllCategories().ToList(),
+                Event = _eventService.GetAllEvents().ToList(),
+                Image = _imageService.GetAllImages().ToList(),
+                Message = _formMessageService.GetAllMessages().ToList()
+            };
+
+            return View(dashboardVM);
         }
 
         public IActionResult PageNotFound()
---
+++ Areas/Admin/ViewModels/DashboardViewModel.cs
@@ -0,0 +1,15 @@
+﻿using CMSWebsite.Models;
+using System.Collections.Generic;
+
+namespace CMSWebsite.Areas.Admin.ViewModels
+{
+    public class DashboardViewModel
+    {
+        public IEnumerable<Album> Album { get; set; }
+        public IEnumerable<Category> Category { get; set; }
+        public IEnumerable<Event> Event { get; set; }
+        public IEnumerable<Image> Image { get; set; }
+        public IEnumerable<Registration> Registration { get; set; }
+        public IEnumerable<FormMessage> Message { get; set; }
+    }
+}
