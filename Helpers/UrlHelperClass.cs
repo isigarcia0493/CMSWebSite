c2433aa592a8c1dede5b052ca90640bd987386e2
Isigarcia0493 <ig705295@southeast.edu>
2023-03-29 16:36:32 -0500

    layout updated

---
+++ Helpers/UrlHelperClass.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Web.Mvc;
+
+namespace CMSWebsite.Helpers
+{
+    public static class UrlHelperClass
+    {
+        public static string IsActive(this IHtmlHelper htmlHelper, string controller, string action)
+        {
+            var routeData = htmlHelper.ViewContext.RouteData;
+
+            var routeAction = routeData.Values["action"].ToString();
+            var routeController = routeData.Values["controller"].ToString();
+
+            var returnActive = (controller == routeController && action == routeAction);
+
+            return returnActive ? "Active" : "";
+        }
+    }
+}
