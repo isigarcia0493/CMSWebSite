91ffa3ddaaadf391f9bdcd81f402c58227afeb5f
Isigarcia0493 <ig705295@southeast.edu>
2023-04-02 09:55:26 -0500

    Events page added

---
+++ Controllers/EventController.cs
@@ -0,0 +1,39 @@
+﻿using CMSWebsite.ServiceInterfaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace CMSWebsite.Controllers
+{
+    public class EventController : Controller
+    {
+        private readonly IEventService _eventSetvice;
+
+        public EventController(IEventService eventSetvice)
+        {
+            _eventSetvice = eventSetvice;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var events = _eventSetvice.GetAllEvents().ToList();
+
+            return View(events);
+        }
+
+        [HttpGet]
+        public IActionResult EventDetails(int id)
+        {
+            if(id != 0)
+            {
+                var eventModel = _eventSetvice.GetEventById(id);
+
+                return View(eventModel);
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
